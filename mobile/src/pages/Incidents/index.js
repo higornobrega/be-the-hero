c299828647309d76d0a79055cc6809ef8264a64f
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-04-17 17:33:05 -0300

    Carregando todos casos

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -10,14 +10,28 @@ export default function incidents() {
     const [incidents, setIncidents] = useState([]);
     const navigation = useNavigation();
     const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] =  useState(false);
 
     function navigateToDetail(incident) {
         navigation.navigate('Detail', { incident });      
     }
     async function loadIncidents() {
-        const response = await api.get('incidents');
-        setIncidents(response.data);
+        if (loading) {
+            return;
+        }
+        if (total > 0 && incidents.length === total) {
+            return;
+        }
+
+        setLoading(true);
+        const response = await api.get('incidents',{
+            params: { page }
+        });
+        setIncidents([... incidents, ... response.data]);
         setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
 
     }
     useEffect(()=> {
@@ -38,6 +52,8 @@ export default function incidents() {
                 data={incidents}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 style={styles.incidentList}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>

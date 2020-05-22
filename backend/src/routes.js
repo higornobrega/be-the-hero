747ac6909e01a39b4b32db450032ee3531eb31b7
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-05-22 12:44:12 -0300

    Validação do par criar ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -9,8 +10,16 @@ const routes = express.Router();
 
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
+routes.post('/ongs',celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),   
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);

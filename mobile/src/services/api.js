d88db63e5dc593d5dc80f398516c7937b59511e6
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-11-26 15:35:43 -0300

    Atualização de pacotes e informações

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.9:3333'
+    baseURL: 'http://192.168.43.54:3333' //IPv4 do PC "Local" (Windows usar o comando ipconfig) ou nuvem
 });
 
 export default api;
